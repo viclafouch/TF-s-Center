6dcb71d4f45aeebbd03bf9a0586e2fffac576989
Victor de la Fouchardière <victor.dlf@outlook.fr>
2018-07-14 15:04:40 +0200

    version 1.15

--- src/js/containers/SearchesContainer/index.js
+++ src/js/containers/SearchesContainer/index.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NewSearches } from '../../components/Searches/New/NewSearches';
 import { ListSearches } from '../../components/Searches/List/ListSearches';
 import { YouTubeContext } from '../../main';
 
-export class SearchesContainer extends Component {
-    render() {
-        return (
-            <div className="searches-container container-scrollable scrollBarOnHover">
-                <div className="pdi--10">
-                    <YouTubeContext.Consumer>
-                        {(context) => (
-                            <NewSearches context={context} />
-                        )}
-                    </YouTubeContext.Consumer>
-                </div>
-                <div className="pdi--10">
-                    <YouTubeContext.Consumer>
-                        {(context) => (
-                            <React.Fragment>
-                               { context.state.searches.length > 0 && <ListSearches /> }
-                            </React.Fragment>
-                        )}
-                    </YouTubeContext.Consumer>
-                </div>
+const SearchesContainer = () => {
+    return (
+        <div className="searches-container container-scrollable scrollBarOnHover">
+            <div className="pdi--10">
+                <YouTubeContext.Consumer>
+                    {(context) => (
+                        <NewSearches context={context} />
+                    )}
+                </YouTubeContext.Consumer>
             </div>
-        )
-    }
+            <div className="pdi--10">
+                <YouTubeContext.Consumer>
+                    {(context) => (
+                        <React.Fragment>
+                            {context.state.searches.length > 0 && <ListSearches />}
+                        </React.Fragment>
+                    )}
+                </YouTubeContext.Consumer>
+            </div>
+        </div>
+    )
 }
 
 export default SearchesContainer
