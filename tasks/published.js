cd69dead43e0428b07c294fd8333f309676b73f4
Victor <victor@seald.io>
2018-11-24 19:07:32 +0100

    add published script

---
+++ tasks/published.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+const AdmZip = require('adm-zip');
+const emoji = require('node-emoji');
+const colors = require('colors');
+
+const directory = 'dist';
+const zip = new AdmZip();
+
+const zipFileName = `${directory}/TF-Center-${+new Date}.zip`
+const files = [ 'popup.html', 'src/build', 'README.md', 'manifest.json', 'background.js']
+const messageOk = `${emoji.get('package')}  Zip file ready to be published on ${colors.blue.underline('https://chrome.google.com/u/1/webstore/devconsole')} !`
+const messageNotOk = `${emoji.get('rotating_light')}  ${colors.red('An error occured while zipping files !')} ${emoji.get('rotating_light')}`
+
+async function removeFiles(directory) {
+  await fs.readdir(directory, async (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) { throw err }
+      });
+    }
+  });
+}
+
+async function published() {
+  try {
+    await removeFiles(directory);
+
+    files.forEach(async path => {
+      const p = fs.statSync(path);
+      if (p.isFile()) {
+        await zip.addLocalFile(path);
+      } else if (p.isDirectory()) {
+        await zip.addLocalFolder(path, path);
+      }
+    });
+
+    zip.writeZip(zipFileName);
+    console.log(messageOk);
+  } catch (error) {
+    console.log(messageNotOk);
+    console.log(`${colors.red('------------')}`)
+    console.log(`${colors.red(error)}`)
+    console.log(`${colors.red('------------')}`)
+  }
+}
+
+published()
