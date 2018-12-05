81e35a47d5049a9239fd1a1d977e80dea16055e9
Victor <victor@seald.io>
2018-12-06 00:33:26 +0100

    filterPeriods async is working

--- src/js/components/Tools/FilterPeriod/FilterPeriod.js
+++ src/js/components/Tools/FilterPeriod/FilterPeriod.js
@@ -1,60 +1,64 @@
 import React, { Component } from 'react'
 import Select from '@components/layouts/Select';
 import { getUrlParameter, updateQueryStringParameter } from '@utils'
+import { Redirect } from 'react-router'
 
 export class FilterPeriod extends Component {
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.periods = [
-            { value: 'anytime', title: 'Anytime' },
-            { value: 'hour', title: 'Last hour' },
-            { value: 'today', title: 'Today' },
-            { value: 'week', title: 'This week' },
-            { value: 'month', title: 'This month' },
-            { value: 'year', title: 'This year' },
-        ]
+    this.periods = [
+      { value: 'anytime', title: 'Anytime' },
+      { value: 'hour', title: 'Last hour' },
+      { value: 'today', title: 'Today' },
+      { value: 'week', title: 'This week' },
+      { value: 'month', title: 'This month' },
+      { value: 'year', title: 'This year' },
+    ]
 
-        this.state = {
-            filters: 'anytime'
-        }
-
-        this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      redirectTo: null,
+      filters: 'anytime'
     }
 
-    componentDidMount() {
-        let filters = getUrlParameter('filters');
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-        return this.setState((prevState) => {
-            return {
-                filters: filters || prevState.filters
-            };
-        });
-    }
+  componentDidMount() {
+    const filters = getUrlParameter('filters')
+    return this.setState((prevState) => ({
+      filters: filters || prevState.filters
+    }))
+  }
 
-    handleChange(e) {
-        let period = e.target.value;
-        if (!this.periods.find(x => x.value == period) || this.props.disabled) return;
-        let url = updateQueryStringParameter(window.location.href, 'filters', period)
-        return window.location.href = url;
-    }
+  componentDidUpdate() {
+    if (this.state.redirectTo) return this.setState({ redirectTo: null })
+  }
 
-    render() {
-        return (
-            <div className={'tool tools-filter-period ' + (this.props.disabled ? 'tool-disabled' : '')}>
-                <Select
-                    blue
-                    disabled={this.props.disabled}
-                    value={this.state.filters}
-                    onChange={this.handleChange}
-                    name="filterPeriod"
-                    options={this.periods}
-                    noEmptyOption
-                />
-            </div>
-        )
-    }
+  handleChange(e) {
+    const period = e.target.value
+    if (!this.periods.find(x => x.value == period) || this.props.disabled) return
+    const url = updateQueryStringParameter(window.location.pathname + window.location.search, 'filters', period)
+    return this.setState({ redirectTo: url })
+  }
+
+  render() {
+    if (this.state.redirectTo) return <Redirect to={this.state.redirectTo} />
+    return (
+      <div className={'tool tools-filter-period ' + (this.props.disabled ? 'tool-disabled' : '')}>
+        <Select
+          blue
+          disabled={this.props.disabled}
+          value={this.state.filters}
+          onChange={this.handleChange}
+          name="filterPeriod"
+          options={this.periods}
+          noEmptyOption
+        />
+      </div>
+    )
+  }
 }
 
 export default FilterPeriod
