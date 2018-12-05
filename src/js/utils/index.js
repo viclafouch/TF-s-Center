86c59421d8c9d30c7c405cf0d974ea14b3120888
Victor <victor@seald.io>
2018-12-05 18:27:53 +0100

    add redirectToWebCache

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -13,6 +13,8 @@ export function getUrlParameter(sParam) {
     }
 };
 
+export const redirectToWebCache = (link, newTab = true) => window.open(`http://webcache.googleusercontent.com/search?q=cache:${link}`, newTab ? '_blank' : '')
+
 export const setStateAsync = (state, self) =>  new Promise(resolve => self.setState(state, resolve))
 
 export const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
