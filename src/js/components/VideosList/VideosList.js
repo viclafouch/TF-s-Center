64e8dd058fa0f87abc7482b98af739a9bce84595
Victor <victor@seald.io>
2019-02-16 12:22:18 +0100

    fix issue CORS on Chrome 73 beta

--- src/js/components/VideosList/VideosList.js
+++ src/js/components/VideosList/VideosList.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
-import VideoListItem from './VideoListItem';
-import { withRouter } from "react-router";
+import VideoListItem from './VideoListItem'
+import { withRouter } from 'react-router'
 import Popup from '../Popup/Popup'
 import Video from '@shared/models/Video.class'
-import VideoDetail from '../VideoDetail/VideoDetail';
-import Loader from '../layouts/Loader';
-import { YouTubeContext } from '@stores/YouTubeContext';
-import { fetchYouTubeChannel, fetchYouTubeVideo } from '@shared/api/YouTube';
-import { redirectToWebCache, setStateAsync, wrapURLs, randomId } from '@utils/index';
+import VideoDetail from '../VideoDetail/VideoDetail'
+import Loader from '../layouts/Loader'
+import { YouTubeContext } from '@stores/YouTubeContext'
+import { fetchYouTubeChannel, fetchYouTubeVideo } from '@shared/api/YouTube'
+import {
+  redirectToWebCache,
+  setStateAsync,
+  wrapURLs,
+  randomId
+} from '@utils/index'
 
 export class VideosList extends Component {
-
   constructor() {
     super()
 
@@ -19,9 +23,9 @@ export class VideosList extends Component {
       isLoading: false
     }
 
-    this.containerScroller = React.createRef();
-    this.videoDetail = React.createRef();
-    this.descriptionDetail = React.createRef();
+    this.containerScroller = React.createRef()
+    this.videoDetail = React.createRef()
+    this.descriptionDetail = React.createRef()
     this.handleChange = this.handleChange.bind(this)
     this.checkedVideo = this.checkedVideo.bind(this)
   }
@@ -32,7 +36,11 @@ export class VideosList extends Component {
    */
   async getInfoVideo(video) {
     if (!video.id) return
-    if (video.isRemoved) return redirectToWebCache(`https://www.youtube.com/watch?v=${video.id}`, true)
+    if (video.isRemoved)
+      return redirectToWebCache(
+        `https://www.youtube.com/watch?v=${video.id}`,
+        true
+      )
 
     try {
       await setStateAsync({ isLoading: true }, this)
@@ -40,15 +48,18 @@ export class VideosList extends Component {
       const videoSelected = await fetchYouTubeVideo(video.id)
       videoSelected.selected = selected
       videoSelected.channel = await fetchYouTubeChannel(videoSelected.channelId)
-
-      let description = wrapURLs(videoSelected.description, true);
-      description = description.replace(/(?:\r\n|\r|\n)/g, '<br>');
+      let description = wrapURLs(videoSelected.description, true)
+      description = description.replace(/(?:\r\n|\r|\n)/g, '<br>')
       this.descriptionDetail.current.innerHTML = description
       await setStateAsync({ videoSelected }, this)
       await this.props.context.openModal('video-detail')
     } catch (error) {
       this.props.context.setState({
-        notification: { id: randomId(), type: 'getVideo', params: { level: 'error', message: error.message } },
+        notification: {
+          id: randomId(),
+          type: 'getVideo',
+          params: { level: 'error', message: error.message }
+        }
       })
 
       if (error.id === 'ERROR_GET_VIDEO_DELETED') {
@@ -56,7 +67,12 @@ export class VideosList extends Component {
         const videoTargetIndex = videosToFlag.findIndex(x => x.id === video.id)
         if (videoTargetIndex !== -1) {
           videosToFlag.splice(videoTargetIndex, 1)
-          this.props.context.setState({ videosToFlag, videosDisplayed: this.props.context.state.onToFlag ? videosToFlag : this.props.context.state.videosDisplayed })
+          this.props.context.setState({
+            videosToFlag,
+            videosDisplayed: this.props.context.state.onToFlag
+              ? videosToFlag
+              : this.props.context.state.videosDisplayed
+          })
         }
       }
     } finally {
@@ -69,7 +85,8 @@ export class VideosList extends Component {
    * @param {Object} prevProps
    */
   componentDidUpdate(prevProps) {
-    if (prevProps.location.key !== this.props.location.key) this.containerScroller.current.scrollTop = 0
+    if (prevProps.location.key !== this.props.location.key)
+      this.containerScroller.current.scrollTop = 0
   }
 
   /**
@@ -91,22 +108,31 @@ export class VideosList extends Component {
     e.stopPropagation()
     const id = e.target.id
     const video = this.props.videos.find(x => x.id === id)
-    if (video && this.props.canFlag) return this.props.onSelect(video, e.target.checked)
+    if (video && this.props.canFlag)
+      return this.props.onSelect(video, e.target.checked)
     return
   }
 
   render() {
     const { videos } = this.props
     return (
-      <div className="container-list scrollBarOnHover main-body" ref={this.containerScroller}>
+      <div
+        className="container-list scrollBarOnHover main-body"
+        ref={this.containerScroller}
+      >
         {this.state.isLoading && <Loader />}
         <YouTubeContext.Consumer>
-          {(context) => (
-            <ul className={"videos-list pdi--top-0 " + (context.state.displaying === 'column' ? 'byColumns' : 'byRows')}>
+          {context => (
+            <ul
+              className={
+                'videos-list pdi--top-0 ' +
+                (context.state.displaying === 'column' ? 'byColumns' : 'byRows')
+              }
+            >
               {videos.map((elem, index) => {
                 return (
                   <li key={index}>
-                    { this.props.canFlag &&
+                    {this.props.canFlag && (
                       <input
                         type="checkbox"
                         id={elem.id}
@@ -117,18 +143,21 @@ export class VideosList extends Component {
                         onChange={this.handleChange}
                         style={{
                           position: 'absolute',
-                          top: (context.state.displaying === 'column' ? 2 : 3),
-                          left: (context.state.displaying === 'column' ? 2 : 3)
+                          top: context.state.displaying === 'column' ? 2 : 3,
+                          left: context.state.displaying === 'column' ? 2 : 3
                         }}
                       />
-                    }
+                    )}
                     <VideoListItem
                       video={elem}
                       onSelect={() => this.getInfoVideo(elem)}
-                      onCheck={e => this.props.canFlag && this.checkedVideo(e, elem)}
+                      onCheck={e =>
+                        this.props.canFlag && this.checkedVideo(e, elem)
+                      }
                     />
-                </li>
-              )})}
+                  </li>
+                )
+              })}
             </ul>
           )}
         </YouTubeContext.Consumer>
@@ -136,8 +165,8 @@ export class VideosList extends Component {
           ref={this.videoDetail}
           maxWidth={1100}
           type="video-detail"
-          callbackOnClosed={() => this.setState({videoSelected: new Video()})
-        }>
+          callbackOnClosed={() => this.setState({ videoSelected: new Video() })}
+        >
           <VideoDetail
             ref={this.descriptionDetail}
             canFlag={this.props.canFlag}
--- src/js/shared/api/YouTube.js
+++ src/js/shared/api/YouTube.js
@@ -1,18 +1,17 @@
-import { YOUTUBE_API_KEYS } from "@private";
-import Video from "@shared/models/Video.class";
-import { TF_ERROR } from "@utils/index";
+import { YOUTUBE_API_KEYS } from '@private'
+import Video from '@shared/models/Video.class'
+import { TF_ERROR } from '@utils/index'
+import { sendMessageToBackground } from '@utils/browser'
 
 /**
  * Fetch a YouTube channel
  * @param {int} id - ID of the channel
  * @return object
  */
-export const fetchYouTubeChannel = channelID => fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelID}&key=${YOUTUBE_API_KEYS}`)
-  .then(response => response.json())
-  .then(response => {
-    if (!response.items) throw new Error()
-    if (!response.items.length) throw new TF_ERROR('ERROR_GET_CHANNEL')
-    return response.items[0].snippet
+export const fetchYouTubeChannel = channelID =>
+  sendMessageToBackground('fetchYouTubeChannel', { channelID }).then(result => {
+    if (result.error) throw new TF_ERROR(result.error.id)
+    return result
   })
 
 /**
@@ -20,11 +19,8 @@ export const fetchYouTubeChannel = channelID => fetch(`https://www.googleapis.co
  * @param {int} videoID - ID of the video
  * @return Video
  */
-export const fetchYouTubeVideo = videoID => fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoID}&key=${YOUTUBE_API_KEYS}`)
-    .then(response => response.json())
-    .then(response => {
-      if (!response.items) throw new Error()
-      if (!response.items.length) throw new TF_ERROR('ERROR_GET_VIDEO_DELETED')
-      return response.items[0].snippet
-    })
-    .then(video => new Video({ ...video, id: videoID }))
+export const fetchYouTubeVideo = async videoID =>
+  sendMessageToBackground('fetchYouTubeVideo', { videoID }).then(result => {
+    if (result.error) throw new TF_ERROR(result.error.id)
+    return new Video({ ...result, id: videoID })
+  })
